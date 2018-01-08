79fd3410011ba12e2b0410d5f1a57271a7bf9648
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2018-01-08 17:55:09 +0100

    added old image basic tools

---
+++ src/vplants/image/serial/all.py
@@ -0,0 +1,20 @@
+# -*- python -*-
+#
+#       vplants.image.serial.all
+#
+#       Copyright 2006 - 2011 INRIA - CIRAD - INRA
+#
+#       File author(s): Daniel Barbeau <daniel.barbeau@inria.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://vplants.gforge.inria.fr
+################################################################################
+
+__license__= "Cecill-C"
+__revision__ = " $Id$ "
+
+from basics import *
+from inrimage import *
